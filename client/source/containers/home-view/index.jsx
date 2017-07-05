4e96c0d6a523ee7dca05dda90063e302e42cb146
Benjamin DEKENS <ben.dekens@gmail.com>
2017-07-06 00:09:16 +0200

    Create action to make money transfer between two accounts

--- client/source/containers/home-view/index.jsx
+++ client/source/containers/home-view/index.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { getAccountBalance, makeMoneyTransfer } from '../../actions/transferMoney';
 
 import HelloCard from '../../components/hello-card';
 
 class HomeView extends Component {
+  componentWillMount() {
+    this.props.getAccountBalance();
+
+    const fromUser = 'tnguyen@gmail.com';  // Should be one of : nsavois@gmail.com, aambal@gmail.com', bdekens@gmail.com, tnguyen@gmail.com
+    const toUser = 'nsavois@gmail.com';    // Should be one of : nsavois@gmail.com, aambal@gmail.com', bdekens@gmail.com, tnguyen@gmail.com
+    const amount = '1.00';
+    const transactionLabel = 'ABC';
+    this.props.makeMoneyTransfer(fromUser, toUser, amount, transactionLabel);
+  }
+
   render() {
-    console.log(this.props);
     const { authentication = {} } = this.props;
-    console.log("authentication",authentication);
+
     return (
       <div className="container">
         <div className="row center-xs">
@@ -28,6 +39,14 @@ function mapStateToProps(state) {
   };
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    getAccountBalance: bindActionCreators(getAccountBalance, dispatch),
+    makeMoneyTransfer: bindActionCreators(makeMoneyTransfer, dispatch),
+  };
+}
+
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(HomeView);
