ddca677dc9b8d9b3a69f54d6140ed81b87310760
souvir <ambalaurelie@gmail.com>
2017-07-06 00:50:17 +0200

    PIK

--- client/source/containers/addExpense/index.jsx
+++ client/source/containers/addExpense/index.jsx
@@ -70,8 +70,8 @@ class Expense extends Component {
   }
   render() {
     return (
-      <div style={styles.container}>
-        <h1>Expense</h1>
+      <div className={styles.container}>
+        <h1>Ajouter une dépense</h1>
 
               <TextField
                 hintText="name"
@@ -112,7 +112,7 @@ class Expense extends Component {
                   <input type="checkbox"
                     onChange={e => this.handleUserSelect(e, user.username)}
                   />
-                  <img src={"./"+user.username} />
+                  <img src={user.pik} />
                   {user.firstName}
                 </li>
               ))}
--- server/boot/fake-data.js
+++ server/boot/fake-data.js
@@ -3,7 +3,7 @@
 module.exports = function (server) {
   server.dataSources.db.automigrate('project', function(err) {
     if(err) throw err;
-    
+
     server.models.project.create([{
       id: 1,
       name: "Renovons la maison familiale",
@@ -17,13 +17,14 @@ module.exports = function (server) {
 
   server.dataSources.db.automigrate('sguser', function(err) {
     if(err) throw err;
-    
+
     server.models.sguser.create([{
       username: 'nsavois',
       email: 'nsavois@gmail.com',
       password: '1234',
       firstName: 'Nicolas',
       lastName: 'Savois',
+      pik: 'https://lh5.googleusercontent.com/-xoWiDa_ZUl8gJRaC59HR4bxXSmHG3nbkSi26yboPiMyzRgXdsZej-YSqfL6_buzLZa_77eIKH7yifQ=w1920-h915',
       roles:[
         {name: "ADMIN"}
       ]
@@ -33,6 +34,7 @@ module.exports = function (server) {
       password: '1234',
       firstName: 'Aurelie',
       lastName: 'Ambal',
+      pik: 'https://lh5.googleusercontent.com/CqL_d3MK4kH1An6-m5xPzXaH1kzU1wJPTTpEHCCqOHOsviCJKHkT-KByUfdmmK35gpHPhjJYO3LazyM=w1920-h915',
       roles:[
         {name: "ADMIN"}
       ]
@@ -42,6 +44,7 @@ module.exports = function (server) {
       password: '1234',
       firstName: 'Benjamin',
       lastName: 'Dekens',
+      pik: 'https://lh4.googleusercontent.com/LMkFln0Tfcn9C2BMkYJou_AHkwK4BFJIj4yfFDCBZ528Mt4RZ4iSmEzY5-77l5UDIRga8Hsj3BP9ek8=w1920-h915',
       roles:[
         {name: "ADMIN"}
       ]
@@ -51,6 +54,7 @@ module.exports = function (server) {
       password: '1234',
       firstName: 'Thang',
       lastName: 'Nguyen',
+      pik: 'https://lh5.googleusercontent.com/fg9vTNIbfjG-TMvLkwnAPUyN3jRw0qkD5sR8EH23GrR3UVPeyVs60HhUa-LGk94TIKdREfmQrTom7OU=w1920-h915',
       roles:[
         {name: "ADMIN"}
       ]
@@ -60,7 +64,7 @@ module.exports = function (server) {
   });
   server.dataSources.db.automigrate('ProjectUser', function(err) {
     if(err) throw err;
-    
+
     server.models.ProjectUser.create([{
       userId: 1,
       projectId: 1
@@ -79,7 +83,7 @@ module.exports = function (server) {
   });
   server.dataSources.db.automigrate('expense', function(err) {
     if(err) throw err;
-    
+
     server.models.expense.create([{
       name:'first expense',
 			date: new Date(),
@@ -94,7 +98,7 @@ module.exports = function (server) {
   });
   server.dataSources.db.automigrate('ExpenseRecipient', function(err) {
     if(err) throw err;
-    
+
     server.models.ExpenseRecipient.create([{
       recipientId: 2,
       expenseId: 1
