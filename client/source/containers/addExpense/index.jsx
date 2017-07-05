427961a8f9f95eb5cfb111e4b4dd1f9f349bd30f
souvir <ambalaurelie@gmail.com>
2017-07-05 21:40:34 +0200

    expense menage

--- client/source/containers/addExpense/index.jsx
+++ client/source/containers/addExpense/index.jsx
@@ -10,6 +10,8 @@ import Toggle from 'material-ui/Toggle';
 
 import FlatButton from 'material-ui/FlatButton';
 
+import styles from './index.css';
+
 import * as ExpenseActions from '../../actions/expense';
 
 class Expense extends Component {
@@ -18,7 +20,6 @@ class Expense extends Component {
     super(props);
 
     this.state = {
-
       name: '',
 
       date: moment().format('YYYY-MM-DD'),
@@ -29,28 +30,15 @@ class Expense extends Component {
 
       recipients: '',
 
-      settled: true,
+      settled: false,
 
       errorText: {
 
-
           name: '',
-
-
 
           date: '',
 
-
-
           amount: '',
-
-
-
-
-
-
-
-
       },
     };
   }
@@ -68,55 +56,18 @@ class Expense extends Component {
     if (e) {
       e.preventDefault();
     }
-    if (value === undefined || value === '') {
-      const errorText = this.state.errorText;
-      errorText[key] = key + ' is required';
-      this.setState({ errorText })
-    } else {
-      const errorText = this.state.errorText;
-      errorText[key] = '';
-      this.setState({ errorText })
-    }
     this.setState({
       [key]: value,
     });
   };
 
   render() {
-    const styles = {
-      container: {
-        padding: '10px',
-      },
 
-      creationContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'left',
-        justifyContent: 'flex-start',
-      },
-
-      property: {
-        margin: '0px 15px 0px 0px',
-      },
-
-
-      toggle: {
-        margin: '0px 15px 0px 0px',
-        width: 'auto',
-      }
-
-
-    };
     return (
       <div style={styles.container}>
         <h1>Expense</h1>
 
-        <div style={styles.creationContainer}>
-
               <TextField
-
-                errorText={this.state.errorText['name']}
-
                 hintText="name"
                 onChange={(e, value) => this.handleChange(e, 'name', value)}
                 style={styles.property}
@@ -133,9 +84,6 @@ class Expense extends Component {
 
 
               <TextField
-
-                errorText="This field is required"
-
                 hintText="amount"
                 onChange={(e, value) => this.handleChange(e, 'amount', value)}
                 style={styles.property}
@@ -145,7 +93,6 @@ class Expense extends Component {
 
 
               <TextField
-
                 hintText="payer"
                 onChange={(e, value) => this.handleChange(e, 'payer', value)}
                 style={styles.property}
@@ -154,21 +101,11 @@ class Expense extends Component {
 
 
               <TextField
-
                 hintText="recipients"
                 onChange={(e, value) => this.handleChange(e, 'recipients', value)}
                 style={styles.property}
                 value={this.state.recipients}
               />
-
-
-              <Toggle
-                label="settled"
-                onToggle={(e, value) => this.handleChange(e, 'settled', value)}
-                style={styles.toggle}
-                value={this.state.settled}
-              />
-
 
           <FlatButton
             label="Submit"
@@ -176,61 +113,7 @@ class Expense extends Component {
           />
         </div>
 
-        <Table> selectable={false} >
-          <TableHeader displaySelectAll={false}>
-            <TableRow>
-              <TableHeaderColumn>id</TableHeaderColumn>
 
-              <TableHeaderColumn>name</TableHeaderColumn>
-
-              <TableHeaderColumn>date</TableHeaderColumn>
-
-              <TableHeaderColumn>amount</TableHeaderColumn>
-
-              <TableHeaderColumn>payer</TableHeaderColumn>
-
-              <TableHeaderColumn>recipients</TableHeaderColumn>
-
-              <TableHeaderColumn>settled</TableHeaderColumn>
-
-            </TableRow>
-          </TableHeader>
-          <TableBody deselectOnClickaway={false} >
-            {_.map(this.props.expenses, (data, index) => (
-              <TableRow key={index}>
-                <TableRowColumn>
-                  { data.id }
-                </TableRowColumn>
-
-                <TableRowColumn>
-                  { data.name }
-                </TableRowColumn>
-
-                <TableRowColumn>
-                  { data.date }
-                </TableRowColumn>
-
-                <TableRowColumn>
-                  { data.amount }
-                </TableRowColumn>
-
-                <TableRowColumn>
-                  { data.payer }
-                </TableRowColumn>
-
-                <TableRowColumn>
-                  { data.recipients }
-                </TableRowColumn>
-
-                <TableRowColumn>
-                  { data.settled }
-                </TableRowColumn>
-
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
     );
   }
 }
