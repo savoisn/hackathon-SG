a0b305783953dcd86a971507fe5c8bc2b7aa360e
ben.dekens <ben.dekens@gmail.com>
2017-07-06 10:24:59 +0200

    Make balance summary

--- client/source/containers/home-view/index.jsx
+++ client/source/containers/home-view/index.jsx
@@ -7,9 +7,9 @@ import HelloCard from '../../components/hello-card';
 
 class HomeView extends Component {
   componentWillMount() {
-    const fromUser = 'tnguyen@gmail.com';  // Should be one of : nsavois@gmail.com, aambal@gmail.com', bdekens@gmail.com, tnguyen@gmail.com
-    const toUser = 'nsavois@gmail.com';    // Should be one of : nsavois@gmail.com, aambal@gmail.com', bdekens@gmail.com, tnguyen@gmail.com
-    const amount = '1.00';
+    const fromUser = 'tnguyen@gmail.com';  // Should be one of : nsavois@gmail.com, aambal@gmail.com', bdekens@gmail.com, tnguyen@gmail.com, seller@fakeshop.com
+    const toUser = 'seller@fakeshop.com';    // Should be one of : nsavois@gmail.com, aambal@gmail.com', bdekens@gmail.com, tnguyen@gmail.com, seller@fakeshop.com
+    const amount = '5.21';
     const transactionLabel = 'ABC';
     // this.props.makeMoneyTransfer(fromUser, toUser, amount, transactionLabel); // UNCOMMENT to make transfer
   }
--- client/source/containers/solde/index.jsx
+++ client/source/containers/solde/index.jsx
@@ -13,9 +13,89 @@ import * as ExpenseActions from '../../actions/expense';
 import styles from './index.css';
 
 class Solde extends Component {
+  getUserBalance = (userId, expenses, sgUsers) => {
+    let userTotalSpend = 0;
+    let projectTotalSpend = 0;
+    _.map(expenses, (expense) => {
+      projectTotalSpend += expense.amount;
+      if (userId === expense.PayerId) {
+        userTotalSpend += expense.amount;
+      }
+    });
+    let userBalance = userTotalSpend - (projectTotalSpend / sgUsers.length);
+    userBalance = userBalance.toFixed(2);
+    return userBalance;
+  }
+
+  getTotalSpent = (userId, expenses) => {
+    let projectTotalSpend = 0;
+    _.map(expenses, (expense) => {
+      projectTotalSpend += expense.amount;
+    });
+    return projectTotalSpend;
+  }
+
+  getExpenseSummary = (sgUsers, expenses) => {
+    let expenseSummary = {};
+    let totalCost = 0;
+    _.map(sgUsers, (user) => (
+      expenseSummary[user.id] = {
+        name: user.firstName + ' ' + user.lastName,
+        pic: user.pik,
+        amount: 0,
+      }
+    ));
+    _.map(expenses, (expense) => (
+      expenseSummary[expense.PayerId].amount += expense.amount,
+      totalCost += expense.amount
+    ));
+    return expenseSummary;
+  }
+
   handleBalanceClick = () => {
     console.log('handle balance');
     // Calcul des soldes
+    const userBalance = this.getUserBalance(
+      this.props.authentication.user.userId,
+      this.props.expenses,
+      this.props.sgUsers,
+    );
+
+    const expenseSummary = this.getExpenseSummary(this.props.sgUsers, this.props.expenses);
+    console.log('expenseSummary : ', expenseSummary);
+
+    const projectTotalSpent = this.getTotalSpent(
+      this.props.authentication.user.userId,
+      this.props.expenses,
+    );
+    console.log('projectTotalSpent : ', projectTotalSpent);
+
+    const balanceSummary = _.map(expenseSummary, (expense) => {
+      const balanceObject = {};
+      const shouldHavePaid = parseFloat(projectTotalSpent) / Object.keys(expenseSummary).length;
+      const balance = expense.amount - shouldHavePaid;
+      const twoDecimalBalance = parseFloat(Math.round(balance * 100) / 100).toFixed(2);
+      balanceObject.balance = twoDecimalBalance;
+      balanceObject.name = expense.name;
+      switch (expense.name) {
+        case 'Nicolas Savois':
+          balanceObject.email = 'nsavois@gmail.com';
+          break;
+        case 'Aurelie Ambal':
+          balanceObject.email = 'aambal@gmail.com';
+          break;
+        case 'Benjamin Dekens':
+          balanceObject.email = 'bdekens@gmail.com';
+          break;
+        case 'Thang Nguyen':
+          balanceObject.email = 'tnguyen@gmail.com';
+          break;
+        default:
+          break;
+      }
+      return balanceObject;
+    });
+    console.log('balanceSummary : ', balanceSummary);
 
     // Call API
   }
@@ -37,6 +117,9 @@ Solde.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    expenses: state.expense,
+    authentication: state.authentication,
+    sgUsers: state.sgusers,
   };
 }
 
