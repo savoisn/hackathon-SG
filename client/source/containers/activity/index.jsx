9930206247d0016c246403204c33df21f5749f8e
Benjamin DEKENS <ben.dekens@gmail.com>
2017-07-06 02:41:11 +0200

    Generate activity logs

--- client/source/containers/activity/index.jsx
+++ client/source/containers/activity/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -10,9 +11,24 @@ import styles from './index.css';
 import * as ExpenseActions from '../../actions/expense';
 
 class Activity extends Component {
+  componentWillMount() {
+    this.props.expenseActions.getExpense();
+  }
   render() {
+    console.log('users : ', this.props.sgUsers);
+    console.log('expenses : ', this.props.expenses);
     return (
-      <h1>Liste des dépenses</h1>
+      <div>
+        <ul className={styles.list}>
+          {_.map(this.props.expenses, (expense, index) => (
+            <li key={index}>
+              <label htmlFor={index}>
+                <img src={this.props.sgUsers.filter(user => user.id === expense.PayerId)[0].pik} /> {this.props.sgUsers.filter(user => user.id === expense.PayerId)[0].firstName} {this.props.sgUsers.filter(user => user.id === expense.PayerId)[0].lastName} {expense.name} {parseFloat(Math.round(parseFloat(expense.amount) * 100) / 100).toFixed(2)}€ {moment(expense.date).format('L')}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
@@ -22,12 +38,14 @@ Activity.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    expenses: state.expense,
     sgUsers: state.sgusers,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
+    expenseActions: bindActionCreators(ExpenseActions, dispatch),
   };
 }
 
